e37eb3be758eb54a995b48972875883714660767
nariyahlucas826 <nariyah.lucas826@students.cmcss.net>
2025-12-17 11:57:47 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
+party_pizza_mini = 14
+print(f"{party_pizza_mini}")
 
+people += 1
+share = party_pizza_mini // people
+leftover = party_pizza_mini % people
+print(share)
+print(leftover)
+
+ people += 2 .
+share = party_pizza_mini // people
+leftover = party_pizza_mini % people
+print(share)
+print(leftover)
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+party_pizza_mini
+share = party_pizza_mini // people
+leftover = party_pizza_mini % people
+print(leftover)
+print(leftover)
+print("...for Mr. Hollow Leg")
