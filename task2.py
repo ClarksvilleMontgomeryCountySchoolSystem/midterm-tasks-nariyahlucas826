1aa65734284d09c12499f4fa61312f7f1026f374
nariyahlucas826 <nariyah.lucas826@students.cmcss.net>
2025-12-17 11:41:45 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,9 @@
+# Week 1: Chores and first purchase
+allowance += dishes
+allowance += lawn
+allowance -= candy
 
+# Week 2: Bonus week and purchase
+allowance *= 3
+allowance += vacuum
+allowance -= toy
